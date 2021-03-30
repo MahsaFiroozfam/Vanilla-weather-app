93a1c594b843c035828d1abea66249354c75be1f
Mahsa Firoozfam <mahsa.firoozfam@gmail.com>
2021-03-30 20:20:03 +0200

    Update current time and date

--- src/app.js
+++ src/app.js
@@ -1,3 +1,17 @@
+function formatDate(timestamp){
+    let date = new Date(timestamp);
+    let hours =date.getHours();
+    if(hours< 10){
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if(minutes<10){
+        minutes = `0${minutes}`;
+    }
+    let days =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let day =days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+}
 function displayTemperature(response){
     
     let cityElement = document.querySelector("#city");
@@ -6,11 +20,14 @@ function displayTemperature(response){
     let feelsLikeElement = document.querySelector("#feels-like");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
+    let dateElement =document.querySelector("#date");
     temperatureElement.innerHTML=Math.round(response.data.main.temp);
+    cityElement.innerHTML= response.data.name;
     descriptionElement.innerHTML=response.data.weather[0].description;
     feelsLikeElement.innerHTML=`Feels Like : ${Math.round(response.data.main.feels_like)}`;
     humidityElement.innerHTML =`Humidity: ${response.data.main.humidity}%`;
     windElement.innerHTML = `Wind: ${Math.round(response.data.wind.speed)} m/s`;
+    dateElement.innerHTML = formatDate(response.data.dt*1000);   
 }
 let apiKey = "d467c6c12588add63695214f8af05053";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`; 
